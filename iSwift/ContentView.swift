1383a921961c35c178dac8eb3ea9f38305df8dc4
Miguel Teixeira <mteixeira88013@gmail.com>
2020-11-11 14:56:27 +0000

    Initial Commit

---
+++ iSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSwift
+//
+//  Created by Miguel Teixeira on 11/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iSwift/iSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSwiftApp.swift
+//  iSwift
+//
+//  Created by Miguel Teixeira on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct iSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
